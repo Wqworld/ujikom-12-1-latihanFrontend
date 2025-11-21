e5df340b8bbbe847b7a7e77d3e6e2cc6567381da
Waqqir Humaid <waqqirhumaid1@gmail.com>
2025-11-21 19:01:08 +0700

    first commit

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,112 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import api from "@/lib/axios";
+import { AxiosError } from "axios";
+import Image from "next/image";
+import {  useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { toast } from "sonner";
+
+export default function LoginPage() {
+  const router = useRouter();
+  const [username, setUsername] = useState(""); 
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    setError("");
+
+    try {
+      //tembak ke backend
+      const response = await api.post("/auth/login", {
+        username,
+        password,
+      });
+      //ambil tokennya dan simpen di localstorage
+      const { token } = response.data;
+      localStorage.setItem("token", token);
+
+      router.push("/dashboard");
+      toast.success("Login Berhasil");
+    } catch (err) {
+      // Kita cek apakah error ini beneran dari Axios?
+      if (err instanceof AxiosError) {
+        // Sekarang TypeScript tahu ini AxiosError, jadi .response boleh diakses
+        const msg = err.response?.data?.message;
+        setError(msg || "Login Gagal");
+      } else {
+        // Kalau error lain (misal codingan error)
+        setError("Terjadi kesalahan sistem");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="w-full h-screen justify-center items-center flex bg-[#EDEDED] ">
+      <div className="md:w-220 md:h-100 flex border-[#68868C] border-3 rounded-xl overflow-hidden">
+        <div className="bg-[#3E3E3E] rounded-lg max-w-60 w-full justify-center items-center flex flex-col text-center outline-3 outline-[#68868C]">
+          <Image
+            src="/assets/mainLogo.png"
+            alt="logo"
+            width={1000}
+            height={1000}
+            className="max-w-25"
+          />
+          <h1 className="font-bold text-xl text-white">KasirKu</h1>
+          <h2 className="font-bold text-[10px] text-white">
+            Kasir Optimal untuk sajian istimewa
+          </h2>
+        </div>
+        <div className="w-full m-15 mx-20 border-l-5 border-[#405559] px-5 overflow-hidden">
+          <p className="text-xl font-bold text-[#405559]">
+            SELAMAT DATANG KEMBALI!
+          </p>
+          <p className="text-sm font-medium text-[#535555] ">
+            Login untuk memberikan pelayanan terbaik atau kelola restoran Anda
+            dengan data yang akurat.
+          </p>
+          {error && (
+            <p className="text-white text-sm bg-red-500/60 rounded-sm p-1">
+              {error}
+            </p>
+          )}
+          <div className="px-15">
+            <div className="w-full h-full mt-5">
+              <form onSubmit={handleSubmit} className="space-y-3">
+                <Input
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="border-[#68868C] border-2 focus:border-[#68868C] focus:ring-[#68868C] text-[#4d4d4d]"
+                />
+
+                <Input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="border-[#68868C] border-2 focus:border-[#68868C] focus:ring-[#68868C]  text-[#4d4d4d]"
+                />
+
+                <Button
+                  type="submit"
+                  className="w-full h-9 bg-[#68868C] rounded-md text-white font-semibold text-sm hover:bg-[#405559] transition duration-300 ease-in-out"
+                >
+                  {loading ? "Loading..." : "Login"}
+                </Button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
